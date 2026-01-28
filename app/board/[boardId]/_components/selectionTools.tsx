f2568a10919ebbd039d2659f0234c84d8ffa41ea
sudarshan-bhavikatte <sudarshanbhavikatte@gmail.com>
2026-01-28 19:05:45 +0530

    added layer depth moving

--- app/board/[boardId]/_components/selectionTools.tsx
+++ app/board/[boardId]/_components/selectionTools.tsx
@@ -8,7 +8,7 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 interface SelectionToolsProps {
     camera: Camera;
     setLastUsedColor: (color: Color) => void;
@@ -18,6 +18,44 @@ export const SelectionTools = memo(({ camera, setLastUsedColor }: SelectionTools
 
     const selection = useSelf((self) => self.presence.selection);
     const selectionBounds = useSelectionBounds();
+
+    const moveToBack = useMutation((
+        { storage }
+    ) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+                indices.push(i);
+            }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+            liveLayerIds.move(indices[i], i);
+        }
+    }, [selection]);
+
+    const moveToFront = useMutation((
+        { storage }
+    ) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toArray();
+
+        for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+                indices.push(i);
+            }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+            liveLayerIds.move(indices[i], arr.length - 1 - (indices.length - 1 - i));
+        }
+    }, [selection])
 
     const setFill = useMutation((
         { storage },
@@ -54,6 +92,18 @@ export const SelectionTools = memo(({ camera, setLastUsedColor }: SelectionTools
             <ColorPicker
                 onChange={setFill}
             />
+            <div className="flex flex-col gap-y-0.5">
+                <Hint label="Bring to front">
+                    <Button variant="board" size="icon" onClick={moveToFront}>
+                        <BringToFront />
+                    </Button>
+                </Hint>
+                <Hint label="Send to back">
+                    <Button variant="board" size="icon" onClick={moveToBack}>
+                        <SendToBack />
+                    </Button>
+                </Hint>
+            </div>
             <div
                 className="flex items-center pl-2 ml-2 border-l border-neutral-200"
             >
